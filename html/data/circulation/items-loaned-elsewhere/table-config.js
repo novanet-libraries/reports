c6b55db0e9aa2eb27fc2f012ed103d83d46fbdf7
Brad Dewar <brad.dewar@novanet.ca>
2019-09-26 10:47:12 -0300

    initial attempt at items-loaned-elsewhere

---
+++ html/data/circulation/items-loaned-elsewhere/table-config.js
@@ -0,0 +1,80 @@
+novanet.datatableInitObject = {
+  order: [[14,"asc"],[13,"asc"]],
+  deferRender: true,
+  columns: [
+    {
+      data: "TITLE",
+      title: "Title"
+    },{
+      data: "AUTHOR",
+      title: "Author",
+      visible: false
+    },{
+      data: "IMPRINT",
+      title: "Imprint",
+      visible: false
+    },{
+      data: "YEAR",
+      title: "Year",
+      visible: false
+    },{
+      data: "ISN",
+      title: "ISSN/ISBN",
+      visible: false,
+      render: novanet.fn.render.isn(),
+      type: "string",
+      excelType: "s"
+    },{
+      data: "BIB_NUMBER",
+      title: "BIB number",
+      visible: false,
+      type: "string",
+      excelType: "s"
+    },{
+      data: "COLLECTION",
+      title: "Collection"
+    },{
+      data: "CALLNUMBER",
+      title: "Callnumber",
+      className: "text-nowrap",
+      type: "callnumber"
+    },{
+      data: "DESCRIPTION",
+      title: "Item Description",
+      visible: false,
+      type: "natural"
+    },{
+      data: "BARCODE",
+      title: "Barcode",
+      type: "string",
+      className: "text-nowrap",
+      render: novanet.fn.render.barcode(),
+      excelType: "s"
+    },{
+      data: "ITEM_STATUS",
+      title: "Item Status",
+      visible: false,
+      type: "string",
+      excelType: "s"
+    },{
+      data: "PROCESS_STATUS",
+      title: "Item Process Status",
+      visible: false
+    },{
+      data: "MATERIAL_TYPE",
+      title: "Material Type"
+    },{
+      data: "LOAN_LOCATION",
+      title: "Loan Location",
+      type: "string",
+      excelType: "s"
+    },{
+      data: "LOAN_DATE",
+      title: "Loan Date",
+      className: "text-nowrap",
+      render: novanet.fn.render.date("MMM Do, YYYY"),
+      excelType: "d",
+      excelFmt: "mmm d, yyyy"
+    }
+  ]
+};
