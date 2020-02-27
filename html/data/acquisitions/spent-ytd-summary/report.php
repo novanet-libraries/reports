4cf0e365411e33cd544d062787e22b0aff0454e3
Brad Dewar <brad.dewar@novanet.ns.ca>
2020-02-27 11:19:05 -0400

    whitespace

--- html/data/acquisitions/spent-ytd-summary/report.php
+++ html/data/acquisitions/spent-ytd-summary/report.php
@@ -29,27 +29,27 @@ foreach($validBudgets[$orderUnit][$year] as $budget => $name){
   $budgetNumbers[] = $budget . "-" . $year;
 }
 
-  try{
+try{
   $cache = new ReportsCache(basename(__DIR__));
-    $maxAge = $year < date('Y')-1 ? 'P1Y' : 'P7D';
-    if ($cache->isStale($maxAge)){
+  $maxAge = $year < date('Y')-1 ? 'P1Y' : 'P7D';
+  if ($cache->isStale($maxAge)){
 
-      $budgetString = "('" . join("','", $budgetNumbers) . "')";
-      $sql = str_replace(":BUDGETS", $budgetString, file_get_contents('./query.sql'));
-      $aleph = new AlephOracle(AlephOracle::LIVE);
-      $cache->refresh(
-        $aleph->query($sql, $bind),
-        $aleph->querySingle("SELECT TO_CHAR(MAX(last_mviews_refresh), 'YYYY-MM-DD HH24:MI:SS') FROM webreport.last_mviews_refresh")
-      );
-    }
+    $budgetString = "('" . join("','", $budgetNumbers) . "')";
+    $sql = str_replace(":BUDGETS", $budgetString, file_get_contents('./query.sql'));
+    $aleph = new AlephOracle(AlephOracle::LIVE);
+    $cache->refresh(
+      $aleph->query($sql, $bind),
+      $aleph->querySingle("SELECT TO_CHAR(MAX(last_mviews_refresh), 'YYYY-MM-DD HH24:MI:SS') FROM webreport.last_mviews_refresh")
+    );
+  }
 
-    $output = $cache->fetch();
-  }
-  catch (Exception $ex){
-    error_log($ex->getMessage());
-    header('HTTP/1.1 500 Internal Server Error');
-    $output = array('error' => $ex->getMessage());
-  }
+  $output = $cache->fetch();
+}
+catch (Exception $ex){
+  error_log($ex->getMessage());
+  header('HTTP/1.1 500 Internal Server Error');
+  $output = array('error' => $ex->getMessage());
+}
 
 
 echo json_encode($output);
