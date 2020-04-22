724b51aba5016ea33c0ce79948bc996cbfa983bf
Brad Dewar <brad.dewar@novanet.ca>
2020-04-22 15:20:35 -0300

    first attempt

---
+++ html/data/circulation/email-list/report.php
@@ -0,0 +1,51 @@
+<?php
+
+require "ReportsCache.class.php";
+require "AlephOracle.class.php";
+require "AlephData.class.php";
+
+header("Content-type: application/json");
+
+$validSublibraries = AlephData::sublibraries();
+$validCodes = array();
+$prohibited = '/^NSHA|NOVA|WWW|.+NET|.+BK$/';
+if (empty($validSublibraries)){
+  header("HTTP/1.1 500 Internal Server Error");
+  echo json_encode(array("error" => "Fetching init data failed."));
+  die();
+}
+foreach($validSublibraries as $code => $text){
+  if (!preg_match($prohibited, $code)){
+    $validCodes[] = $code;
+  }
+}
+
+$sublibrary = strtoupper($_GET["sublibrary"]);
+if (empty($sublibrary) || !in_array($sublibrary, $validCodes)){
+  header("HTTP/1.1 400 Bad Request");
+  $output = array("error" => "Invalid sublibrary code", "valid" => $validCodes);
+}
+else{
+  try{
+    $cache = new ReportsCache(basename(__DIR__));
+
+    if ($cache->isStale()){
+      $sql     = file_get_contents("./query.sql");
+      $bind    = array(":SUBLIBRARY" => $sublibrary);
+
+      $aleph = new AlephOracle(AlephOracle::LIVE);
+      $cache->refresh(
+        $aleph->query($sql, $bind)
+      );
+    }
+
+    $output = $cache->fetch();
+  }
+  catch (Exception $ex){
+    error_log($ex->getMessage());
+    header("HTTP/1.1 500 Internal Server Error");
+    $output = array("error" => $ex->getMessage());
+  }
+}
+
+echo json_encode($output);
---
+++ html/data/circulation/email-list/table-config.js
@@ -0,0 +1,15 @@
+novanet.datatableInitObject = {
+  order: [[1,"asc"]],
+  columns: [
+    {
+      data : 'EMAIL',
+      title: 'Email',
+      className: 'text-nowrap'
+    },{
+      data : 'BOR_STATUS',
+      title: 'Patron Status',
+      type: 'string',
+      excelType: 's'
+    }
+  ]
+};
