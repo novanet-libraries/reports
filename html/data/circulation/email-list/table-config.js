c45041d952465668589c33aff66f4a5146d0aeaf
Brad Dewar <brad.dewar@novanet.ca>
2020-04-22 15:26:28 -0300

    use correct column names in table config

--- html/data/circulation/email-list/table-config.js
+++ html/data/circulation/email-list/table-config.js
@@ -2,11 +2,11 @@ novanet.datatableInitObject = {
   order: [[1,"asc"]],
   columns: [
     {
-      data : 'EMAIL',
+      data : 'Z304_EMAIL_ADDRESS',
       title: 'Email',
       className: 'text-nowrap'
     },{
-      data : 'BOR_STATUS',
+      data : 'Z305_BOR_STATUS',
       title: 'Patron Status',
       type: 'string',
       excelType: 's'
